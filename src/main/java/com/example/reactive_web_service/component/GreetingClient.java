47a06af39671d6a46856cae3a1042b595181be5d
Akatofff <super.kostya.a@yandex.ru>
2025-02-06 15:15:15 +0300

    first commit

---
+++ src/main/java/com/example/reactive_web_service/component/GreetingClient.java
@@ -0,0 +1,26 @@
+package com.example.reactive_web_service.component;
+
+import com.example.reactive_web_service.Greeting;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.net.http.WebSocket;
+
+@Component
+public class GreetingClient {
+
+    private final WebClient client;
+
+    public GreetingClient(WebClient.Builder builder) {
+        this.client = builder.baseUrl("http://localhost:8080").build();
+    }
+
+    public Mono<String> getMessage() {
+        return this.client.get().uri("/hello").accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Greeting.class)
+                .map(Greeting::getMessage);
+    }
+}
